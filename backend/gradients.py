2e0e16cd7e493435a70c785e8b4b64ef761d6572
verstandinvictus <peter.k.cawley@gmail.com>
2016-08-03 16:50:45 -0700

    hmm zat vas vierd

--- backend/gradients.py
+++ backend/gradients.py
@@ -5,8 +5,8 @@ red = Color(rgb=(86/255, 77/255, 143/255))
 yellow = Color(rgb=(170/255, 83/255, 151/255))
 green = Color(rgb=(233/255, 145/255, 45/255))
 
-ry = red.range_to(yellow, 12)
-yg = yellow.range_to(green, 12)
+ry = red.range_to(yellow, 30)
+yg = yellow.range_to(green, 30)
 gradient = [red.hex] + [x.hex for x in ry] + [yellow.hex] + \
            [x.hex for x in yg] + [green.hex]
 
